3f9f7b24d918590d4d30981587a2715ce737c3b5
HARSHAVARDHAN-TNK <tnkharsha@gmail.com>
2025-10-15 17:31:18 +0530

    added requirements.txt and changed port in app.py

--- backend/app.py
+++ backend/app.py
@@ -101,4 +101,5 @@ def not_found(e):
     return jsonify({"error": "Route not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
